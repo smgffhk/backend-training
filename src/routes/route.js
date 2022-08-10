b065d1c8a3502e929fbcd389a56e1928a16b8e8a
suraj kumar <106535205+smgffhk@users.noreply.github.com>
2022-08-10 23:10:44 +0530

    queryparam

--- src/routes/route.js
+++ src/routes/route.js
@@ -36,6 +36,34 @@ router.get('/candidates/:canidatesName', function(req, res){
     res.send('Done')
 })
 
+app.get("/sol1", function (req, res) {
+    let arr= [1,2,3,5,6,7]
+    let sumofnumber= (arr[arr.lenght-1]*(arr[arr.lenght-1]+1))/2
+    someofArr= arr.reduce((a,b) => a+b)
+    let missingNumber = sumofnumber-someofArr
+    res.send(  { data: missingNumber  }  );
+  });
+
+
+router.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+           
+    let arr= [33, 34, 35, 37, 38]
+    let length= arr.length
+   
+    sumOfArr = arr.reduce((a,b)=> a+b)
+   
+    let first= arr[0]
+    let last= arr.pop()
+    let consecutiveSum= (length + 1) * (first+ last ) / 2
+    let missingNumber= consecutiveSum - sumOfArr
+   
+    res.send(  { data: missingNumber  }  );
+   
+   })
+
+
+
 
 module.exports = router;
 // adding this comment for no reason
